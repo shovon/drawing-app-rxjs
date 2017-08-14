857dff1e02fa79f38f062fb62f9b9b7b5162b16d
Salehen Shovon Rahman <salehen.rahman@gmail.com>
2017-08-14 08:08:46 -0700

    Documenting the code.

---
+++ README.md
@@ -0,0 +1,7 @@
+# RxJS drawing pad example
+
+A drawing pad. Allows users to draw things.
+
+Uses RxJS for composing the HTML5 canvas' `mousedown`, `mousemove`, and `mouseup` events. Without RxJS, we would need to manually maintain states. But with event composition, the state does not need to be manually maintained, allowing for code that is easier to reason about, and also much easier to test.
+
+Take a look at `script.js` to see how it all looks.
--- script.js
+++ script.js
@@ -11,6 +11,9 @@ const getMousePos = (canvas, evt) => {
   };
 }
 
+// The `mousedown`, `mouseup`, `mousemove` events turned into an Rx Observable
+// stream.
+
 const mousedown = Rx.Observable
   .fromEvent(canvas, 'mousedown')
   .map((e) => ({ clickType: 'mousedown', position: getMousePos(canvas, e) }));
@@ -23,13 +26,24 @@ const mousemove = Rx.Observable
   .fromEvent(canvas, 'mousemove')
   .map((e) => getMousePos(canvas, e));
 
+// Merge the `mousedown` and `mouseup` streams into a single sequential stream.
+
 const clicks = Rx.Observable.merge(mousedown, mouseup);
 
+// Combine the `clicks` stream and `mousemove` stream into a stream of events
+// where both the click and move are combined into a single event.
+//
+// This will be useful to us when we want to handle the event when the user
+// clicks and drags the cursor across the screen for drawing.
 const clickMove = clicks.combineLatest(mousemove, ({ clickType }, position) => {
   return { clickType, position };
 });
 
+// Only get the click+move event after a mousedown event, and ignore all those
+// that come after a mouseup event.
 const dragDown = clickMove.filter(({ clickType }) => clickType === 'mousedown');
+
+// Fairly self-explanatory below.
 
 mousedown.subscribe(({ position }) => {
   const { x, y } = position;
